7db352b282a64bc09a96546cfeee5568eb5baf96
Jeremy <youremail@gmail.com>
2025-10-03 12:46:44 -0400

    Project Part 2

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ name = input("Before we begin, what is your character's name? \n> ")
 
 # Print the name
 print(f"Great, {name}! Let's begin the adventure!")
+
+player = {
+    'name': name,
+    'health': 100,
+    'coin': 0
+}
+import random
+
+events = ["find a coin", "meet a monster", "do nothing"]
+event = random.choice(events)
+
+print("While exploring, you {event}!")
+
+if event == "find a coin":
+    player['coin'] += 1
+    print(f"After finding a coin, {player['name']} now has {player['coin']} coins.")
+elif event == "meet a monster":
+    player['health'] -= 10
+    print(f"After meeting a monster, {player['name']} now has {player['health']} health.")
+elif event == "do nothing":
+    print(f"{player['name']} did nothing")    
