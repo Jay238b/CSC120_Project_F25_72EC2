5f397670452dc44d1c9a8f3edc74f3b89fc23c73
Jeremy <youremail@gmail.com>
2025-10-03 12:02:40 -0400

    Project Part 1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 game_name = "Escaping" #TODO, name your game project
-print()
-print("======================")
+print(f"Welcome to {game_name}!")
+print("====================")
 
 # Ask for the character name
-name = input()
+name = input("Before we begin, what is your character's name? \n> ")
 
 # Print the name
-print()
+print(f"Great, {name}! Let's begin the adventure!")
