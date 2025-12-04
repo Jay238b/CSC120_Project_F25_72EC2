a356b48cebf5eee61d967c38591823e33caf8dcf
Jeremy <youremail@gmail.com>
2025-12-04 13:41:36 -0500

    Completed Project Part 4

--- game.py
+++ game.py
@@ -9,8 +9,6 @@ class Player:
         self.coin = 0
 
     def move(self, direction, map_size):
-        old_x, old_y = self.x, self.y
-
         if direction == "s": 
             if self.x < map_size - 1:
                 self.x += 1
@@ -33,27 +31,21 @@ class Player:
         else: 
             print("You cannot move that way!")
         
-        if (old_x, old_y) == (self.x, self.y) and direction in "wasd":
-            if not(
-                (direction == "s" and old_x < map_size - 1) or
-                (direction == "w" and old_x > 0) or
-                (direction == "d" and old_y < map_size - 1) or
-                (direction == "a" and old_y > 0)
-            ):
-                pass
 
 class GameMap:
-    def __init__(self):
-        self.size = 9
+    def __init__(self, size=9):
+        self.size = size
     
     def draw(self, player):
-        for j in range(self.size):
+        for i in range(self.size):
             row = ""
-            for i in range(self.size):
+            for j in range(self.size):
                 if i == player.x and j == player.y:
-                    row += "C "
+                    row += "P "
                 elif i == self.size - 1 and j == self.size - 1:
                     row += "M "
+                elif i == 0 and j == 0:
+                    row += "C "
                 else:
                     row += ". "
             print(row)
@@ -62,10 +54,10 @@ class GameMap:
 class Game:
     def __init__(self):
         self.game_name = "Treasure Hunt"
-        self.name = ""
-        self.events = ["find a coin", "meet a monster", "do nothing"]
         self.player = Player()
-        self.map = GameMap()
+        self.map_size = 9
+        self.events = ["find a coin", "meet a monster", "do nothing"]
+        self.map = GameMap(self.map_size)
 
     def check_event(self):
         event = random.choice(self.events)
@@ -76,8 +68,8 @@ class Game:
         elif event == "meet a monster":
             self.player.health -= 10
             print("A monster attacked you!")
-        else: 
-            print("Nothing happens...")
+        elif event == "do nothing":
+            print("Nothing happens.")
     
     def play(self):
         print("Welcome!")
@@ -85,14 +77,21 @@ class Game:
 
         while True:
             self.map.draw(self.player)
+            move = input("Move (w/a/s/d or quit): ").strip().lower()
+            if move == "quit":
+                print("Game over. Thanks for playing!")
+                break
+            self.player.move(move, self.map_size)
+            self.check_event()
 
             if self.player.x == self.map_size - 1 and self.player.y == self.map_size - 1:
                 print("You reached the exit!")
                 break
+            if self.player.health <= 0:
+                print("You lost all health. Game Over!")
 
-            move = input("Move (w/a/s/d): ")
-            self.player.move(move, self.map_size)
-            self.check_event()
+            
+            
 
 if __name__ == "__main__":
     Game().play()
